c8bf06a6a6690c743ab894926de4e93d2019c29b
Bmannava-invn <mannava.flex@gmail.com>
2018-01-09 00:09:31 -0800

    README.md
    initial readme

--- digits-power-sum.js
+++ digits-power-sum.js
@@ -1,40 +1,93 @@
 //Digit Power Sum - 119
-
+//Added to Github (https://github.com/mannava/Puzzles)
 //Iterating the sums and comparing with exponencial power of its sum.
 //when it matches pushing to result object.
-function digitsSum(){
-  var pow = 2, cnt=1, resultObj={};
-  for(var i=2;cnt<=30;i++){
-    for(var j=2;j<=9;j++){
+var Mocha = require('mocha');
+var mocha = new Mocha();
+var assert = require('assert');
 
-      pow = Math.pow(i,j);
-      if(i === sumOfTheDigits(pow)){
-        //console.log(cnt, " ",pow,"-------------------------",i ,"pow", j);
-        resultObj[pow] =  i +"^"+j;
-        cnt++;
-      }
+mocha.suite.emit('pre-require', this, 'solution', mocha);
+
+function digitsPowerSum() {
+    var pow = 2, cnt = 1, resultObj = {};
+    for (var i = 2; cnt <= 30; i++) {
+        for (var j = 2; j <= 9; j++) {
+
+            pow = Math.pow(i, j);
+            if (i === sumOfTheDigits(pow)) {
+                //console.log(cnt, " ",pow,"-------------------------",i ,"pow", j);
+                resultObj[pow] = i + "^" + j;
+                cnt++;
+            }
+        }
     }
-  }
-  var result = Object.keys(resultObj).sort(function(a,b){
-    return a-b;
-  })
-  cnt=1;
-  for(var key in resultObj){
-    console.log("a"+cnt+" = ", result[cnt-1]);
-    cnt++
-  }
+    var result = Object.keys(resultObj).sort(function (a, b) {
+        return a - b;
+    })
+    cnt = 1;
+    for (var key in resultObj) {
+        console.log("a" + cnt + " = ", result[cnt - 1]);
+        cnt++
+    }
+
+    return result;
 
 }
 
-// Returns all matched sums of the digits
-function sumOfTheDigits(num){
-  var sum = 0, num = num.toString();
-  for(var i=0;i<num.length;i++){
-    sum += parseInt(num[i]);
-  }
-  return sum;
+// Returns sums of the digits
+function sumOfTheDigits(num) {
+    var sum = 0, num = num.toString();
+    for (var i = 0; i < num.length; i++) {
+        sum += parseInt(num[i]);
+    }
+    return sum;
 }
 
-console.time("TOTAL_TIME_TAKEN");
-digitsSum();
-console.timeEnd("TOTAL_TIME_TAKEN");
+//console.time("TOTAL_TIME_TAKEN");
+//digitsPowerSum();
+runTest();
+
+//console.timeEnd("TOTAL_TIME_TAKEN");
+
+function runTest() {
+
+//Unit test cases.
+    var output = ['81',
+        '512',
+        '2401',
+        '4913',
+        '5832',
+        '17576',
+        '19683',
+        '234256',
+        '390625',
+        '614656',
+        '1679616',
+        '17210368',
+        '34012224',
+        '52521875',
+        '60466176',
+        '205962976',
+        '612220032',
+        '8303765625',
+        '10460353203',
+        '24794911296',
+        '27512614111',
+        '52523350144',
+        '68719476736',
+        '271818611107',
+        '1174711139837',
+        '2207984167552',
+        '20047612231936',
+        '72301961339136',
+        '248155780267521',
+        '3904305912313344'];
+
+
+    describe('digitsPowerSum', function () {
+        it('should match the output with results for respective digits sum', function () {
+            assert.deepEqual(digitsPowerSum(), output);
+        });
+    });
+    mocha.run();
+}
--- roman-to-roman.js
+++ roman-to-roman.js
@@ -1,83 +1,114 @@
+//Roman numerals   Problem 89
+//Added to Github (https://github.com/mannava/Puzzles)
+
+const Mocha = require('mocha');
+const mocha = new Mocha();
+var assert = require('assert');
+
+mocha.suite.emit('pre-require', this, 'solution', mocha);
+
 var romanObj = {
-  "M": 1000,
-  "CM": 900,
-  "D": 500,
-  "CD": 400,
-  "C": 100,
-  "XC": 90,
-  "L": 50,
-  "XL": 40,
-  "X": 10,
-  "IX": 9,
-  "V": 5,
-  "IV": 4,
-  "I": 1
+    "M": 1000,
+    "CM": 900,
+    "D": 500,
+    "CD": 400,
+    "C": 100,
+    "XC": 90,
+    "L": 50,
+    "XL": 40,
+    "X": 10,
+    "IX": 9,
+    "V": 5,
+    "IV": 4,
+    "I": 1
 };
 
 //Reading given roman numbers textfile.
 function readRomanTxtFile() {
-  var request = require('request');
-  var url = "https://projecteuler.net/project/resources/p089_roman.txt";
-  return new Promise(function (resolve, reject) {
-    request(url, function (error, response, body) {
-      if (response.statusCode === 200) {
-        resolve(response.body);
-      } else {
-        reject(error);
-      }
+    var request = require('request');
+    var url = "https://projecteuler.net/project/resources/p089_roman.txt";
+    return new Promise(function (resolve, reject) {
+        request(url, function (error, response, body) {
+            if (response.statusCode === 200) {
+                resolve(response.body);
+            } else {
+                reject(error);
+            }
 
+        });
     });
-  });
 }
 
 //Extracting  roman number and passing for comparision
+function getPromiseTotalChars() {
+    return readRomanTxtFile().then(function (response) {
+        var romansDataArr = response.split("\n"), len = 0;
+        for (var i = 0; i < romansDataArr.length; i++) {
+            len += savedChars(i, romansDataArr[i]);
+        }
+
+        console.log(" TOTAL NO OF CHARS SAVED : ", len);
+        return len;
+    }, function (error) {
+        console.error("Failed ", error);
+    });
+}
+
 readRomanTxtFile().then(function (response) {
-  console.time("Time_Taken")
-  var romansDataArr = response.split("\n"), len = 0;
-  for (var i = 0; i < romansDataArr.length; i++) {
-    len += savedChars(i, romansDataArr[i]);
-  }
+    var romansDataArr = response.split("\n"), len = 0;
+    for (var i = 0; i < romansDataArr.length; i++) {
+        len += savedChars(i, romansDataArr[i]);
+    }
 
-  console.log(" TOTAL NO OF CHARS SAVED : ", len);
-  console.timeEnd("Time_Taken")
-
+    console.log(" TOTAL NO OF CHARS SAVED : ", len);
+    return len;
 }, function (error) {
-  console.error("Failed ", error);
+    console.error("Failed ", error);
 });
 
 //Finding total charecters that saves.
 function savedChars(cnt, romanStr) {
-  var resultStr = '', len = 0;
-  ;
-  resultStr = Roman2Number(romanStr);
-  len = romanStr.length - resultStr.length;
-  return len;
-  //console.log(cnt , romanStr +" ", resultStr , " total chars saved :", len);
+    var resultStr = '', len = 0;
+    ;
+    resultStr = Roman2Number(romanStr);
+    len = romanStr.length - resultStr.length;
+
+    //console.log(cnt , romanStr +" ", resultStr , " total chars saved :", len);
+    return len;
 }
 
 //savedChars("MMMDLXVIIII");
 
 //Converting passed Roman number to digital format.
 function Roman2Number(romanChars) {
-  var digit = 0, len = romanChars.length;
-  for (var i = 0; i < romanChars.length; i++) {
-    if (romanObj[romanChars[i]] < romanObj[romanChars[i + 1]]) {
-      digit -= romanObj[romanChars[i]]
-    } else {
-      digit += romanObj[romanChars[i]]
+    var digit = 0, len = romanChars.length;
+    for (var i = 0; i < romanChars.length; i++) {
+        if (romanObj[romanChars[i]] < romanObj[romanChars[i + 1]]) {
+            digit -= romanObj[romanChars[i]]
+        } else {
+            digit += romanObj[romanChars[i]]
+        }
     }
-  }
-  return digit2Roman(digit);
+    return digit2Roman(digit);
 }
 
 //Converting again back to passed digital number to Roman numeral.
 function digit2Roman(digit) {
-  var roman = '';
-  for (var key in romanObj) {
-    while (digit >= romanObj[key]) {
-      roman += key;
-      digit -= romanObj[key];
+    var roman = '';
+    for (var key in romanObj) {
+        while (digit >= romanObj[key]) {
+            roman += key;
+            digit -= romanObj[key];
+        }
     }
-  }
-  return roman;
+    return roman;
 }
+
+
+describe('TotalCharsSaved', function () {
+    it('should return total chars saved for the given file.', function () {
+        //assert.deepEqual(getPromiseTotalChars(), '743');
+    });
+});
+mocha.run();
+
